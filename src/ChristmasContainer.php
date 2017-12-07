52284b94be376e42260da40ae162bf6a7ac25306
marcinlacina <marcin.lacina@zslt.klucznet.pl>
2017-12-07 18:00:39 +0100

    added getEndpoint operation to api

--- src/ChristmasContainer.php
+++ src/ChristmasContainer.php
@@ -2,10 +2,10 @@
 
 namespace Syntax;
 
-use Psr\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
 
 class ChristmasContainer
 {
--- web/api.php
+++ web/api.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: marcin
- * Date: 02.12.17
- * Time: 12:21
- */
+
+if(!isset($_GET['ctrl'])) {
+    echo 'Invalid controller!'; exit;
+}
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$container = \Syntax\ChristmasContainer::getInstance();
+
+switch($_GET['ctrl']) {
+    default:
+        echo 'Invalid controller!'; exit;
+
+    case 'getEndpoint':
+        header('Content-Type: application/json');
+        echo json_encode([
+            'endpoint' => preg_match('/192\.168/', $_SERVER['REMOTE_ADDR']) ?
+                $container->getParameter('local_ws_endpoint') : $container->getParameter('external_ws_endpoint')
+        ]);
+        break;
+}
