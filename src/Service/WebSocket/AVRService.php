fb39be2f132db13e264524d342c79dd55778338f
marcinlacina <marcin.lacina@zslt.klucznet.pl>
2017-12-11 18:18:51 +0100

    created AVRService and user in Lights

---
+++ src/Service/WebSocket/AVRService.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Syntax\Service\WebSocket;
+
+use Syntax\Exception\AVRException;
+use Syntax\Model\Application\LogEntity;
+use Syntax\Model\Application\LogEvents;
+use Syntax\Service\Database;
+
+class AVRService
+{
+    /**
+     * @var string
+     */
+    private $host;
+
+    /**
+     * @var int
+     */
+    private $port;
+
+    /**
+     * @var int
+     */
+    private $timeout;
+
+    /**
+     * @var resource
+     */
+    private $tcpHandle;
+
+
+    /**
+     * @var Database
+     */
+    private $database;
+    
+
+    /**
+     * @var null|int
+     */
+    private $__last_connection = null;
+
+    /**
+     * @var int
+     */
+    private $__retry_counter = 0;
+
+    const CONNECTION_EXPIRE_SECONDS = 180;
+    const MAX_RETRY = 20;
+
+    /**
+     * AVRService constructor.
+     * @param string $avrHost
+     * @param int $avrPort
+     * @param int $avrTimeout
+     * @param Database $database
+     * @throws AVRException
+     */
+    public function __construct($avrHost, $avrPort, $avrTimeout, Database $database)
+    {
+        $this->host = $avrHost;
+        $this->port = $avrPort;
+        $this->timeout = $avrTimeout;
+
+        $this->database = $database;
+
+        $this->reopenConnection();
+    }
+
+
+    /**
+     * Lights destructor.
+     */
+    public function __destruct()
+    {
+        if($this->tcpHandle) fclose($this->tcpHandle);
+    }
+
+    /**
+     * Reopen connection to worker module
+     */
+    public function reopenConnection()
+    {
+        if(!$this->__last_connection || time()-$this->__last_connection > static::CONNECTION_EXPIRE_SECONDS) {
+            if($this->tcpHandle) {
+                fclose($this->tcpHandle);
+            }
+
+            $this->tcpHandle = @stream_socket_client('tcp://'.$this->host.':'.$this->port, $err, $errStr, $this->timeout, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT);
+            if (!$this->tcpHandle) {
+                if($this->__retry_counter++ < static::MAX_RETRY) {
+                    $this->addAVRLog(
+                        LogEvents::AVR_ERROR,
+                        sprintf('Cannot connect to AVR module on %s:%s.'.PHP_EOL.'Retrying after 10 seconds..', $this->host, $this->port)
+                    );
+                    sleep(10);
+                    return $this->reopenConnection();
+                }
+
+                $this->addAVRLog(LogEvents::AVR_CRITICAL, sprintf('%s (%s)', $errStr, $err));
+                throw new AVRException(sprintf('Cannot connect to AVR module on %s:%s.', $this->host, $this->port));
+            }
+
+            $this->addAVRLog(LogEvents::AVR_CONNECTED, sprintf('Open connection to worker module on %s:%s', $this->host, $this->port));
+            $this->__last_connection = time();
+        }
+        return null;
+    }
+
+    /**
+     * @param $message
+     * @return int
+     */
+    public function send($message)
+    {
+        return fwrite($this->tcpHandle, $message);
+    }
+
+    /**
+     * @param $logEvent
+     * @param $message
+     */
+    private function addAVRLog($logEvent, $message)
+    {
+        $log = new LogEntity();
+        $log->createdTime = new \DateTime();
+        $log->initiator = __CLASS__.': '.__LINE__;
+        $log->name = $logEvent;
+        $log->data = [
+            'message' => $message
+        ];
+        $this->database->addLog($log);
+    }
+}
--- src/WebSocket/InMemory/Lights.php
+++ src/WebSocket/InMemory/Lights.php
@@ -3,9 +3,7 @@
 namespace Syntax\WebSocket\InMemory;
 
 use Syntax\Exception\AVRException;
-use Syntax\Model\Application\LogEntity;
-use Syntax\Model\Application\LogEvents;
-use Syntax\Service\Database;
+use Syntax\Service\WebSocket\AVRService;
 
 class Lights
 {
@@ -15,89 +13,18 @@ class Lights
     private $storage = [false, false, false, false];
 
     /**
-     * @var resource
+     * @var AVRService
      */
-    private $connector;
-
-    /**
-     * @var Database
-     */
-    private $database;
-
-    /**
-     * @var null|int
-     */
-    private $__last_connection = null;
-
-    /**
-     * @var int
-     */
-    private $__retry_counter = 0;
-
-    const CONNECTION_EXPIRE_SECONDS = 180;
-    const MAX_RETRY = 20;
-    const LIGHTS_STORAGE_SIZE = 4;
-
-    /**
-     * @var array
-     */
-    private $avr = [];
+    private $avr;
 
     /**
      * Lights constructor.
-     * @param string $avrHost
-     * @param int $avrPort
-     * @param int $avrTimeout
-     * @param Database $database
+     * @param AVRService $avr
      * @throws AVRException
      */
-    public function __construct($avrHost, $avrPort, $avrTimeout, Database $database)
+    public function __construct(AVRService $avr)
     {
-        $this->avr['host'] = $avrHost;
-        $this->avr['port'] = $avrPort;
-        $this->avr['timeout'] = $avrTimeout;
-        $this->database = $database;
-
-        $this->reopenConnection();
-    }
-
-    /**
-     * Reopen connection to worker module
-     */
-    private function reopenConnection()
-    {
-        if(!$this->__last_connection || time()-$this->__last_connection > static::CONNECTION_EXPIRE_SECONDS) {
-            if($this->connector) {
-                fclose($this->connector);
-            }
-
-            $this->connector = @stream_socket_client('tcp://'.$this->avr['host'].':'.$this->avr['port'], $err, $errStr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT);
-            if (!$this->connector) {
-                if($this->__retry_counter++ < static::MAX_RETRY) {
-                    $this->addLightsLog(
-                        LogEvents::AVR_ERROR,
-                        sprintf('Cannot connect to AVR module on %s:%s.'.PHP_EOL.'Retrying after 10 seconds..', $this->avr['host'], $this->avr['port'])
-                    );
-                    sleep(10);
-                    return $this->reopenConnection();
-                }
-
-                $this->addLightsLog(LogEvents::AVR_CRITICAL, sprintf('%s (%s)', $errStr, $err));
-                throw new AVRException(sprintf('Cannot connect to AVR module on %s:%s.', $this->avr['host'], $this->avr['port']));
-            }
-
-            $this->addLightsLog(LogEvents::AVR_CONNECTED, sprintf('Open connection to worker module on %s:%s', $this->avr['host'], $this->avr['port']));
-            $this->__last_connection = time();
-        }
-        return null;
-    }
-
-    /**
-     * Lights destructor.
-     */
-    public function __destruct()
-    {
-        if($this->connector) fclose($this->connector);
+        $this->avr = $avr;
     }
 
 
@@ -117,10 +44,10 @@ class Lights
      */
     public function changeState($offset, $value)
     {
-        $this->reopenConnection();
+        $this->avr->reopenConnection();
 
         $this->storage[$offset] = (bool)$value;
-        fwrite($this->connector, 'P'.$this->toByte());
+        $this->avr->send('P'.$this->toByte());
         return $this;
     }
 
@@ -134,7 +61,7 @@ class Lights
         foreach(str_split(sprintf('%04d', decbin($byte))) as $bit) {
             $this->storage[$i++] = (bool)$bit;
         }
-        fwrite($this->connector, 'P'.$this->toByte());
+        $this->avr->send('P'.$this->toByte());
         return $this;
     }
 
@@ -158,21 +85,5 @@ class Lights
             if($bit) $convertedByte += $weight[$i];
         }
         return $convertedByte;
-    }
-
-    /**
-     * @param $logEvent
-     * @param $message
-     */
-    private function addLightsLog($logEvent, $message)
-    {
-        $log = new LogEntity();
-        $log->createdTime = new \DateTime();
-        $log->initiator = __CLASS__.': '.__LINE__;
-        $log->name = $logEvent;
-        $log->data = [
-            'message' => $message
-        ];
-        $this->database->addLog($log);
     }
 }
