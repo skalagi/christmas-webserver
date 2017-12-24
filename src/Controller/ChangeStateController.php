b98e50e33c98e2c64a3d48c5d81b9e80bb85a2b8
lenovo <lenovo@192.168.1.23>
2017-12-24 21:09:45 +0100

    increase worker timeout

--- src/Controller/ChangeStateController.php
+++ src/Controller/ChangeStateController.php
@@ -45,7 +45,7 @@ class ChangeStateController implements ControllerInterface
     private $database;
 
     const BUSY_LIMIT_PER_USER = 15; // max 60
-    const WORKER_TIMEOUT = .5;
+    const WORKER_TIMEOUT =1;
 
     /**
      * LightsController constructor.
