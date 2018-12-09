d906c527aba952f1ebec64ecdd783fc819a1d30c
Syntaxerro <marcin.lacina@zslt.klucznet.pl>
2018-12-09 13:45:40 +0100

    prototype queue adding validation

--- src/Controller/ChangeColorController.php
+++ src/Controller/ChangeColorController.php
@@ -3,6 +3,7 @@
 namespace Syntax\Controller;
 
 use Ratchet\ConnectionInterface;
+use Syntax\Exception\QueueException;
 use Syntax\Model\AbstractModelWithStatus;
 use Syntax\Model\ChangeColor;
 
@@ -17,8 +18,27 @@ class ChangeColorController extends Controller
     public function execute($input, ConnectionInterface &$from)
     {
         $changeColor = new ChangeColor($input);
+        $this->_checkQueuePreviousItemIsTheSame($changeColor);
+
         $changeColor->status = AbstractModelWithStatus::STATUS_ADDED;
         $this->queue->push($changeColor);
         $this->clients->broadcastMessage(json_encode($changeColor));
+    }
+
+    /**
+     * @param ChangeColor $changeColor
+     * @throws QueueException
+     */
+    private function _checkQueuePreviousItemIsTheSame(ChangeColor $changeColor)
+    {
+        $items = $this->queue->getItems();
+        if(!$items) {
+            return;
+        }
+
+        $lastItem = $items[count($items)-1];
+        if($lastItem instanceof ChangeColor && $lastItem->r == $changeColor->r && $lastItem->g == $changeColor->g && $lastItem->b == $changeColor->b) {
+            throw new QueueException('Your change color was ignored cause previous color is the same!');
+        }
     }
 }
--- src/Controller/ChangeStateController.php
+++ src/Controller/ChangeStateController.php
@@ -3,6 +3,7 @@
 namespace Syntax\Controller;
 
 use Ratchet\ConnectionInterface;
+use Syntax\Exception\QueueException;
 use Syntax\Model\AbstractModelWithStatus;
 use Syntax\Model\ChangeState;
 
@@ -17,8 +18,27 @@ class ChangeStateController extends Controller
     public function execute($input,  ConnectionInterface &$from)
     {
         $changeState = new ChangeState($input);
+        $this->_checkQueuePreviousItemIsTheSame($changeState);
+
         $changeState->status = AbstractModelWithStatus::STATUS_ADDED;
         $this->queue->push($changeState);
         $this->clients->broadcastMessage(json_encode($changeState));
+    }
+
+    /**
+     * @param ChangeState $changeState
+     * @throws QueueException
+     */
+    private function _checkQueuePreviousItemIsTheSame(ChangeState $changeState)
+    {
+        $items = $this->queue->getItems();
+        if(!$items) {
+            return;
+        }
+
+        $lastItem = $items[count($items)-1];
+        if($lastItem instanceof ChangeState && $lastItem->status == $changeState->status && $lastItem->channel == $changeState->channel) {
+            throw new QueueException('Your change relay state was ignored cause previous state is the same!');
+        }
     }
 }
