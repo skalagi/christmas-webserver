e9164130ebe2bd9a380ef9be501998dc005bce8f
Syntaxerro <marcin.lacina@zslt.klucznet.pl>
2018-12-23 16:57:41 +0100

    hf

--- rpi/write_relays.sh
+++ rpi/write_relays.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+if [ -z $1 ]
+then
+    $1 = 1
+fi
+
+if [ -z $2 ]
+then
+    $2 = 1
+fi
+
+if [ -z $3 ]
+then
+    $3 = 1
+fi
+
+if [ -z $4 ]
+then
+    $4 = 1
+fi
+
+
 echo $1 > /sys/class/gpio/gpio14/value
 echo $2 > /sys/class/gpio/gpio15/value
 echo $3 > /sys/class/gpio/gpio18/value
